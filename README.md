24f9f1f3ac1ddf5bf011b09818eb5a802a9145fd
ishengfang <nf0126@google.com>
2017-10-01 18:20:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Font 2 Array Generator
+==========================
+
+This repository is a python generator for transfer font to numpy array and for keras's function fit_generator. This is the prepocess of my East Asian Ideograph deep learning project.
+
+Requirments
+-----------
+### Require
+numpy
+PIL
+### Recommend
+Keras
---
+++ font2array.py
@@ -0,0 +1,101 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+import pickle
+try:
+    from keras.preprocessing.image import img_to_array, array_to_img
+except:
+    print(
+        '''
+        Warning! Can not import keras methods!
+        Do Not Use font2arry_keras and font2arry_keras_fit
+        '''
+    )
+
+def Font2Image(hanzi, size, font):
+    im = Image.new("L", (size,size), color=0)
+    draw = ImageDraw.Draw( im )
+    font=ImageFont.truetype( font, size)
+    draw.text ( (0,0), hanzi, fill=255, font=font )
+    return im
+
+def font2arry(font, size, encoding):
+    
+    '''
+    Input font (.ttf), size(int), encoding list(list or string)
+    Yeild numpy array (no chennel ie. shape=(size, size))
+    
+    need:
+         numpy
+         PIL
+    '''
+    
+    for hanzi in chrlist:
+        im    = Font2Image(hanzi, size, font)
+        array = np.array(im)
+        array = array.reshape(size, size)
+        yield array
+        
+def font2arry_chennel(font, size, encoding):
+    
+    '''
+    Input font (.ttf), size(int), encoding list(list or string)
+    Yeild numpy array (have chennel ie. shape=(size, size,1))
+    
+    need:
+         numpy
+         PIL
+    '''
+    
+    for hanzi in chrlist:
+        im    = Font2Image(hanzi, size, font)
+        array = np.array(im)
+        array = array.reshape(size, size, 1)
+        yield array
+
+
+def font2arry_keras(font, size, encoding):
+    
+    '''
+    Input font (.ttf), size(int), encoding list(list or string)
+    Yeild numpy array (have chennel ie. shape=(size, size,1))
+    
+    need:
+         numpy
+         PIL
+         keras
+    '''
+    
+    for hanzi in chrlist:
+        yield img_to_array(Font2Image(hanzi, size, font))
+
+def font2arry_keras_fit(font, size, encoding, batchsize, suffle= True):
+    
+    '''
+    Input font (.ttf), size(int), encoding list(list or string), batchsize
+    Yeild numpy array (have chennel ie. shape=(size, size,1)) for keras fit_generator
+    it will shuffle the chrlist
+    need:
+         numpy
+         PIL
+         keras
+    '''
+    
+    #Make infinite loop for fit_generator
+    while True:
+        
+        suffle_chrlist = list(chrlist)
+        
+        if suffle:
+            np.random.shuffle(suffle_chrlist) #shuffle for fitting
+        else:
+            pass
+        
+        #make a batch array
+        batch_num = len(suffle_chrlist)//batchsize
+        for index in range(batch_num):
+            batch_chrlist = suffle_chrlist[index*batchsize: (index+1)*batchsize]
+            batch_array = []
+            for hanzi in batch_chrlist:
+                batch_array.append(img_to_array(Font2Image(hanzi, size, font)))
+            yield np.array(batch_array), np.array(batch_array)
